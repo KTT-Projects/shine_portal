50bc7160222f53cab3feaf2938884221a550ec1c
Tasuku24 <muscle.monkey2nd@gmail.com>
2024-03-06 14:25:56 +0900

    add "const" before scaffold

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:shine_portal/pages/home_page.dart';
 import 'package:shine_portal/pages/login_page.dart';
 
@@ -14,9 +13,9 @@ class MainPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return HomePage();
+            return const HomePage();
           } else {
-            return LoginPage();
+            return const LoginPage();
           }
         },
       ),
