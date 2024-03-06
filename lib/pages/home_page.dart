2d1c0ede5a9fa84ed6933515caf28ac844835372
Takami-Marsh <t-marsh@kttprojects.com>
2024-03-06 15:38:04 +0900

    Just a backup for the homepage work I've done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,9 +16,6 @@ class _HomePageState extends State<HomePage> {
     String userId = user.email!.replaceAll('@shine.com', '');
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF3E5C79),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -31,6 +29,42 @@ class _HomePageState extends State<HomePage> {
               child: const Text('Sign out'),
             ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+        ]),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(bottom: 20, top: 10, left: 20, right: 20),
+          child: GNav(
+            haptic: true,
+            tabBorderRadius: 35,
+            tabBackgroundColor: const Color(0x2F3E5C79),
+            duration: const Duration(milliseconds: 300),
+            gap: 10,
+            tabs: [
+              const GButton(
+                icon: Icons.home,
+                text: 'ホーム',
+              ),
+              const GButton(
+                icon: Icons.chat,
+                text: 'チャット',
+              ),
+              const GButton(
+                icon: Icons.notifications,
+                text: '通知',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: userId,
+              ),
+            ],
+            selectedIndex: 0,
+            onTabChange: (index) {},
+          ),
         ),
       ),
     );
