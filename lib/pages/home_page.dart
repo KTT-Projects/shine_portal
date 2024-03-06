fc8a72ef0bc8eb84648462b9544f5e2cc926b872
Tasuku24 <muscle.monkey2nd@gmail.com>
2024-03-06 13:40:47 +0900

    delete the appbar in home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
     String userId = user.email!.replaceAll('@shine.com', '');
 
     return Scaffold(
-      appBar: ,
+      // appBar: ,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
