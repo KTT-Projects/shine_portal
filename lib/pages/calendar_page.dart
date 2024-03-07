36c7a6db97d7f2152e693c4400c5272c0cc23fcb
Tasuku24 <muscle.monkey2nd@gmail.com>
2024-03-07 12:30:50 +0900

    add calendar

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -8,10 +11,20 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  DateTime _forcuseDay = DateTime.now();
+  DateTime? _selectedDay;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Calendar')),
+    return Padding(
+      padding: const EdgeInsets.only(top: 45.0, left: 8.0, right: 8.0),
+      child: TableCalendar(
+        headerStyle: HeaderStyle(formatButtonVisible: false),
+        firstDay: DateTime.utc(2024, 1, 1),
+        lastDay: DateTime.utc(2030, 12, 31),
+        focusedDay: _forcuseDay,
+      ),
     );
   }
 }
