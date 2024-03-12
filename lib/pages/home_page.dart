c73a9e1e32439879c6be8675ce56e27315f6ae24
Tasuku24 <muscle.monkey2nd@gmail.com>
2024-03-12 16:46:27 +0900

    change color to relative

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,12 +34,15 @@ class _HomePageState extends State<HomePage> {
     String userId = user.email!.replaceAll('@shine.com', '');
 
     return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: Color(0xFFF0F5FA),
+      appBar: AppBar(
+        toolbarHeight: 0,
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: pages[_selectedIndex],
       bottomNavigationBar: Container(
-        decoration: BoxDecoration(color: Colors.white, boxShadow: [
-          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+        decoration: BoxDecoration(color: Theme.of(context).colorScheme.background, boxShadow: [
+          BoxShadow(blurRadius: 20, color: Theme.of(context).colorScheme.onBackground.withOpacity(.1)),
         ]),
         child: Padding(
           padding:
@@ -47,7 +50,8 @@ class _HomePageState extends State<HomePage> {
           child: GNav(
             haptic: true,
             tabBorderRadius: 35,
-            tabBackgroundColor: const Color(0x2F3E5C79),
+            tabBackgroundColor: Theme.of(context).colorScheme.primary.withOpacity(.1),
+            // tabBackgroundColor: Theme.of(context).colorScheme.primaryContainer,
             duration: const Duration(milliseconds: 300),
             gap: 10,
             mainAxisAlignment: MainAxisAlignment.center,
